2236d2738cfa663d3271eda94b57b5cd8adfda9c
MickelsonKlaus <84320904+MickelsonKlaus@users.noreply.github.com>
2022-02-16 13:46:09 +0100

    Update Header.jsx

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -5,22 +5,22 @@ function Header({ title }) {
         <Head>
             <title>{title}</title>
 <title>Redox - More than anonymous messaging</title>
-<meta name="title" content="Redox - More than anonymous messaging">
-<meta name="description" content="Create anonymous messages and questions, share to friends and earn from every message or answer you receive from them. Also earn from referrals.">
+<meta name="title" content="Redox - More than anonymous messaging" />
+<meta name="description" content="Create anonymous messages and questions, share to friends and earn from every message or answer you receive from them. Also earn from referrals." />
 
 
-<meta property="og:type" content="website">
-<meta property="og:url" content="https://resox-m.vercel.app/">
-<meta property="og:title" content="Redox - More than anonymous messaging">
-<meta property="og:description" content="Create anonymous messages and questions, share to friends and earn from every message or answer you receive from them. Also earn from referrals.">
-<meta property="og:image" content="">
+<meta property="og:type" content="website"/>
+<meta property="og:url" content="https://resox-m.vercel.app/" />
+<meta property="og:title" content="Redox - More than anonymous messaging"/>
+<meta property="og:description" content="Create anonymous messages and questions, share to friends and earn from every message or answer you receive from them. Also earn from referrals."/>
+<meta property="og:image" content=""/>
 
 
-<meta property="twitter:card" content="summary_large_image">
-<meta property="twitter:url" content="https://resox-m.vercel.app/">
-<meta property="twitter:title" content="Redox - More than anonymous messaging">
-<meta property="twitter:description" content="Create anonymous messages and questions, share to friends and earn from every message or answer you receive from them. Also earn from referrals.">
-<meta property="twitter:image" content="">
+<meta property="twitter:card" content="summary_large_image"/>
+<meta property="twitter:url" content="https://resox-m.vercel.app/"/>
+<meta property="twitter:title" content="Redox - More than anonymous messaging"/>
+<meta property="twitter:description" content="Create anonymous messages and questions, share to friends and earn from every message or answer you receive from them. Also earn from referrals."/>
+<meta property="twitter:image" content=""/>
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9139701609499388"
      crossorigin="anonymous"></script>
         </Head>
