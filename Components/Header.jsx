67f3f0d15650a3a769b9b00bbb1f617e3d79913d
MickelsonKlaus <84320904+MickelsonKlaus@users.noreply.github.com>
2022-02-16 15:24:53 +0100

    Update Header.jsx

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -13,14 +13,14 @@ function Header({ title }) {
 <meta property="og:url" content="https://resox-m.vercel.app/" />
 <meta property="og:title" content="Redox - More than anonymous messaging"/>
 <meta property="og:description" content="Create anonymous messages and questions, share to friends and earn from every message or answer you receive from them. Also earn from referrals."/>
-<meta property="og:image" content=""/>
+<meta property="og:image" content="https://resox-m.vercel.app/undraw_New_message_re_fp03.png"/>
 
 
 <meta property="twitter:card" content="summary_large_image"/>
 <meta property="twitter:url" content="https://resox-m.vercel.app/"/>
 <meta property="twitter:title" content="Redox - More than anonymous messaging"/>
 <meta property="twitter:description" content="Create anonymous messages and questions, share to friends and earn from every message or answer you receive from them. Also earn from referrals."/>
-<meta property="twitter:image" content=""/>
+<meta property="twitter:image" content="https://resox-m.vercel.app/undraw_New_message_re_fp03.png"/>
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9139701609499388"
      crossorigin="anonymous"></script>
         </Head>
