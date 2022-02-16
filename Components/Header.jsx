68d6dabd570f687cd3cde050cc199e0e0163a288
MickelsonKlaus <84320904+MickelsonKlaus@users.noreply.github.com>
2022-02-16 15:34:09 +0100

    Update Header.jsx

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -14,7 +14,7 @@ function Header({ title }) {
 <meta property="og:title" content="Redox - More than anonymous messaging"/>
 <meta property="og:description" content="Create anonymous messages and questions, share to friends and earn from every message or answer you receive from them. Also earn from referrals."/>
 <meta property="og:image" content="https://resox-m.vercel.app/undraw_New_message_re_fp03.png"/>
-
+<meta name="keywords" content="resox, anonymous messaging, messages, anonymous questions, create and earn, refer and earn, message, messaging, anonymous"/>
 
 <meta property="twitter:card" content="summary_large_image"/>
 <meta property="twitter:url" content="https://resox-m.vercel.app/"/>
