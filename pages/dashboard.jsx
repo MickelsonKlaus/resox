f4d337ee6ec9cbb34d8209944be59ef5a3b1c8d3
MickelsonKlaus <84320904+MickelsonKlaus@users.noreply.github.com>
2022-02-16 19:35:46 +0100

    Update dashboard.jsx

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -45,7 +45,7 @@ function Dashboard({ user }) {
 
       let messagesRef = collection(doc(userRef, user.uid), "messages");
 
-      let q = query(messagesRef, orderBy("date"));
+      let q = query(messagesRef, orderBy("date", "desc"));
 
       const querySnapshot = await getDocs(q);
       let messagesData = [];
